aaf84666865602066157784e68c91744ea88a2d3
sunggeorge <sunggeorge@gmail.com>
2024-12-06 00:33:02 -0700

    add background effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 "use client";
+import { WavyBackground } from "@/components/ui/wavy-background";
 import { title, subtitle} from "@/components/primitives";
 import {Image} from "@nextui-org/react";
 import { useUser } from "@clerk/nextjs";
 export default function Home() {
   const { user } = useUser();
   return (
+    <WavyBackground backgroundFill="white" className="max-w-4xl mx-auto pb-40 relative overflow-hidden h-[100%]"> 
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+               
               <Image
               width={150}
               height={150}  
               src="/starlight_logo.png"
               alt="Starlight Nails"
               object-fit="cover"
-              />
+              />      
+              
       <div className="inline-block max-w-xl text-center justify-center">
 
         {user && (
@@ -27,7 +31,8 @@ export default function Home() {
           Welcome to Starlight Nails Salon - Where Beauty Meets Perfection
         </span>
       </div>
-
+    
     </section>
+    </WavyBackground>
   );
 }
