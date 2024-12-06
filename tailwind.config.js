0bdf6ed206ee142148daa9cbcdb25175f4e09d07
sunggeorge <sunggeorge@gmail.com>
2024-12-06 00:42:43 -0700

    Fix tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from '@nextui-org/theme';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -17,4 +17,4 @@ module.exports = {
   },
   darkMode: "class",
   plugins: [nextui()],
-}
+};
